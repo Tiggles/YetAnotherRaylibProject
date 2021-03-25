b6055bf5384be87dcb3bce61437b117f43f7443e
Jonas Malte Hinchely <jonas@hinchely.dk>
2021-03-25 18:41:27 +0100

    Refactor. Scrolling + building no longer works. Zoom + build does

--- main.c
+++ main.c
@@ -34,11 +34,14 @@ typedef struct HouseWrapper {
     struct HouseWrapper *next;
 } HouseWrapper;
 
-inline struct House* initHouse(int x, int y);
+struct House* initHouse(int x, int y);
 bool isRectangleColliding(Rectangle rect1, Rectangle rect2);
 HouseWrapper* isAnyColliding(HouseWrapper *housesWrapper,  Vector2 point, int houseWidth, int houseHeight);
 void drawHouses(HouseWrapper *housesWrapper, Vector2 cameraPosition, Texture2D *house);
 void addHouse(HouseWrapper *housesWrapper, Vector2 position);
+void input(Camera2D *camera, float delta, Message *message, HouseWrapper *housesWrapper, int houseWidth, int houseHeight);
+void update(Message *message, float delta);
+void render(void);
 
 int wrappersAmount = 0;
 int screenWidth = 800;
@@ -81,29 +84,25 @@ int main() {
 
     while (!WindowShouldClose()) {
         float delta = GetFrameTime();
-        Vector2 mouse = GetMousePosition();
-        if (message.timeRemaining > 0) message.timeRemaining -= delta;
-
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            if (!isAnyColliding(&housesWrapper, mouse, houseWidth, houseHeight)) {
-                addHouse(&housesWrapper, mouse);
-            } else {
-                message.message = "Cannot place building. Collides with other house.";
-                message.timeRemaining = 5;
-            }
-        }
+        input(&camera, delta, &message, &housesWrapper, houseWidth, houseHeight);
+        update(&message, delta);
         DRAW(
+
             ClearBackground((Color){70, 149, 75});
+            render();
 
             MODE_2D(
                 drawHouses(&housesWrapper, camera.offset, &house);
             );
+            Vector2 mouse = GetMousePosition(); 
+            Vector2 pointerPosition = GetScreenToWorld2D(mouse, camera);
+            DrawCircle(pointerPosition.x, pointerPosition.y, 10, RED);
 
             DrawFPS(10, 10);
 
             DrawRectangle(screenWidth * 0.75f, 0, screenWidth / 4 , 30, WHITE);
             if (message.timeRemaining > 0) {
-                DrawText(message.message, mouse.x, mouse.y, 16, RED);
+                DrawText(message.message, pointerPosition.x, pointerPosition.y, 16, RED);
             }
             char buff[10];
             DrawText("Wrappers: ", 2, 2, 20, WHITE);
@@ -111,37 +110,57 @@ int main() {
             DrawText(itoa(mouse.x, buff, 10), 10, 25, 20, WHITE);
             DrawText(itoa(mouse.y, buff, 10), 10, 40, 20, WHITE);
         );
-
-
-        int scrollSpeed = IsKeyDown(KEY_LEFT_SHIFT) ? 250 : 150;
-
-        if (IsKeyDown(KEY_W)) {
-            camera.offset.y += delta * scrollSpeed;
-        } else if (IsKeyDown(KEY_S)) {
-            camera.offset.y -= delta * scrollSpeed;
-        }
-
-        if (IsKeyDown(KEY_A)) {
-            camera.offset.x += delta * scrollSpeed;
-        } else if (IsKeyDown(KEY_D)) {
-            camera.offset.x -= delta * scrollSpeed;
-        }
-            
-        if (IsKeyPressed(KEY_R)) {
-            camera.offset.x = camera.offset.y = 0;
-            camera.offset.x = camera.offset.y = 0;
-            camera.zoom = 1;
-        }
-            
-        float mouseWheel = GetMouseWheelMove();
-        SetMouseOffset(-camera.offset.x, -camera.offset.y);
-        camera.zoom += mouseWheel / 10;
-        SetMouseScale(camera.zoom, camera.zoom);
-
     }
     CloseWindow(); // Close window and OpenGL context
     return 0;
 }
+
+void update(Message *message, float delta) {
+    if (message->timeRemaining > 0) message->timeRemaining -= delta;
+}
+
+void render() {
+
+}
+
+void input(Camera2D *camera, float delta, Message *message, HouseWrapper *housesWrapper, int houseWidth, int houseHeight) {
+    Vector2 mouse = GetMousePosition();
+    SetMouseOffset(camera->offset.x, camera->offset.y);
+
+    Vector2 pointerPosition = GetScreenToWorld2D(mouse, *camera);
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        if (!isAnyColliding(housesWrapper, pointerPosition, houseWidth, houseHeight)) {
+            addHouse(housesWrapper, pointerPosition);
+        } else {
+            message->message = "Cannot place building. Collides with other house.";
+            message->timeRemaining = 5;
+        }
+    }
+
+
+    int scrollSpeed = IsKeyDown(KEY_LEFT_SHIFT) ? 250 : 150;
+
+    if (IsKeyDown(KEY_W)) {
+        camera->offset.y += delta * scrollSpeed;
+    } else if (IsKeyDown(KEY_S)) {
+        camera->offset.y -= delta * scrollSpeed;
+    }
+
+    if (IsKeyDown(KEY_A)) {
+        camera->offset.x += delta * scrollSpeed;
+    } else if (IsKeyDown(KEY_D)) {
+        camera->offset.x -= delta * scrollSpeed;
+    }
+        
+    if (IsKeyPressed(KEY_R)) {
+        camera->offset.x = camera->offset.y = 0;
+        camera->zoom = 1;
+    }
+
+    float mouseWheel = GetMouseWheelMove();
+    camera->zoom += mouseWheel / 10;
+}
+
 
 inline House* initHouse(int x, int y) {
     House *house = malloc(sizeof(House));
